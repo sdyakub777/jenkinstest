44e1b04902c50a5cfd2e5bc5d3caf2a8269dde18
sdyakub777 <105699189+sdyakub777@users.noreply.github.com>
2022-08-03 14:32:01 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,4 +1,4 @@
 $User = "syed@syed777.onmicrosoft.com"
 $Pword = ConvertTo-SecureString –String 'Yakub@123' –AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $User, $Pword
-New-PowerBIReport -Path 'C:\Syed\WWIDW-Sales.pbix' -Name 'SyedTest'
+New-PowerBIReport -Path 'C:\Syed\WWIDW-Sales.pbix' -Name 'SyedTestJenkins'
