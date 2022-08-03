3724b10a0f7bc7e28ef9f7288202ae780053a5d8
sdyakub777 <105699189+sdyakub777@users.noreply.github.com>
2022-08-03 17:01:53 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -2,4 +2,4 @@
 $Password = $env:Password | ConvertTo-SecureString -AsPlainText -Force
 $creddentials = New-Object System.Management.Automation.PSCredential -ArgumentList $env:UserName, $Password
 Connect-PowerBIServiceAccount -Credential $creddentials
-New-PowerBIReport -Path $env:File -Name 'JJJ'
+New-PowerBIReport -Path '$env:File' -Name 'JJJ'
