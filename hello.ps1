005fcda3f4efa18109422c968676095386522d58
sdyakub777 <105699189+sdyakub777@users.noreply.github.com>
2022-08-03 15:24:12 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,5 +1,5 @@
-$User = "syed@syed777.onmicrosoft.com"
-$Pword = ConvertTo-SecureString –String 'Yakub@123' –AsPlainText -Force
+$User = "%username%"
+$Pword = ConvertTo-SecureString –String '%password%' –AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $User, $Pword
 Connect-PowerBIServiceAccount -Credential $Credential
-New-PowerBIReport -Path 'C:\Syed\WWIDW-Sales.pbix' -Name 'ttt'
+New-PowerBIReport -Path 'C:\Syed\WWIDW-Sales.pbix' -Name 'sss'
