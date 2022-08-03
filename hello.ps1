b393bb360f5d1aa7044a0c05ffbe768a6217d1b1
sdyakub777 <105699189+sdyakub777@users.noreply.github.com>
2022-08-03 16:12:26 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,3 +1,4 @@
-$Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList ${env:username}, ${env:password}
+$Password = $env:password | ConvertTo-SecureString -AsPlainText -Force
+$creddentials = New-Object System.Management.Automation.PSCredential -ArgumentList $env:username, $Password
 Connect-PowerBIServiceAccount -Credential $Credential
-New-PowerBIReport -Path 'C:\Syed\WWIDW-Sales.pbix' -Name 'MMM'
+New-PowerBIReport -Path 'C:\Syed\WWIDW-Sales.pbix' -Name 'JJJ'
